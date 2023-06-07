8f7235179c722c251291e9e17ee51d5e751fa443
marceloaguillar <marceloaguillarjr@gmail.com>
2023-06-07 20:35:33 -0300

    A interface já está enviando os dados para o banco de dados, e na entrada de telefone, foi feito um código, para que já seja formatado para o modo de telefones brasileiros

--- cadastro_cliente.py
+++ cadastro_cliente.py
@@ -1,20 +1,43 @@
 import tkinter as tk
 import sqlite3
 import pandas as pd
+import phonenumbers
 
-#CRIAR BANCO DE DADOS
-#conexao = sqlite3.connect('bancos_clientes.db')
-#cursor = conexao.cursor()
-#cursor.execute(''' CREATE TABLE clientes (
-   # nome text,
-   # sobrenome text,
-   # email text,
-   # telefone text
- #   )
-#''')
+def formatar_telefone(*args):
+    telefone = entry_telefone.get()
+    
+    # Remover todos os caracteres não numéricos do número de telefone
+    telefone = ''.join(filter(str.isdigit, telefone))
+    
+    # Formatar o número de telefone com a formatação brasileira
+    telefone_formatado = ''
+    if telefone:
+        telefone_formatado = phonenumbers.format_number(
+            phonenumbers.parse(telefone, 'BR'),
+            phonenumbers.PhoneNumberFormat.NATIONAL
+        )
+    
+    # Atualizar o campo de entrada (Entry) com o telefone formatado
+    entry_telefone.delete(0, tk.END)
+    entry_telefone.insert(0, telefone_formatado)
 
-#conexao.commit()
-#conexao.close()
+def cadastrar_cliente():
+    
+    nome = entry_nome.get()
+    email = entry_email.get()
+    telefone = entry_telefone.get()
+    conexao = sqlite3.connect('bancos_clientes.db')
+    cursor = conexao.cursor()
+    cursor.execute( '''INSERT INTO clientes (nome, email, telefone) values (?, ?, ?) ''', (nome,email,telefone))
+           
+    conexao.commit()
+    conexao.close()
+    entry_nome.delete(0, tk.END)
+    entry_email.delete(0, tk.END)
+    entry_telefone.delete(0, tk.END)
+
+def exportar_cliente():
+    return
 
 
 janela = tk.Tk()
@@ -39,10 +62,14 @@ entry_email.grid(row=1, column=1, padx=10, pady=10)
 
 entry_telefone = tk.Entry(janela, text='Telefone', width=30)
 entry_telefone.grid(row=2, column=1, padx=10, pady=10)
+entry_telefone.bind('<KeyRelease>', formatar_telefone)
 
-#botões
+#BOTÕES
+botao_cadastrar = tk.Button(janela, text='Cadastrar Cliente', command=cadastrar_cliente)
+botao_cadastrar.grid(row=3, column=0, padx=10, pady=10)
 
-
+botao_exportar = tk.Button(janela, text='Exportar banco de dados', command=exportar_cliente)
+botao_exportar.grid(row=3, column=1, padx=10, pady=10)
 
 janela.mainloop()
 #CRIAR TABELA
